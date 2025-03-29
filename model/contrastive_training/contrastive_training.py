b8f6f37b3b8e52f816e3a6274d39303b27f747b7
draby2466 <ribhavyadav2466@gmail.com>
2025-03-29 11:57:32 +0530

    Switched to torchmetrics for accuracy measures. Added Otsu thresholding for better threshold value. Added Normalization to transforms.

--- model/contrastive_training/contrastive_training.py
+++ model/contrastive_training/contrastive_training.py
@@ -4,23 +4,26 @@ import time
 import torch.optim as optim
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
-from torch.optim.lr_scheduler import StepLR
+import torchmetrics
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torchvision import transforms
 from model.model import SiameseNetwork
 from torchvision.datasets import ImageFolder
 from model.loss_types import ContrastiveLoss
 from dataset.data_model import ContrastiveDataset
+from skimage.filters import threshold_otsu
 
 transform = transforms.Compose([
     transforms.Grayscale(),
     transforms.Resize((100, 100)),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5], std=[0.5])
 ])
 
 # Hyperparameters
-BATCH_SIZE = 64
-EPOCHS = 15
-MARGIN = 1.5
+BATCH_SIZE = 128
+EPOCHS = 10
+MARGIN = 0.5
 
 data = ImageFolder(root="../../dataset/extracted_faces", transform=transform)
 
@@ -28,8 +31,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = SiameseNetwork().to(device)
 criterion = ContrastiveLoss(margin=MARGIN).to(device)
-optimizer = optim.Adam(model.parameters(), lr=0.01)
-scheduler = StepLR(optimizer, step_size=5, gamma=0.1)
+optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
+scheduler = ReduceLROnPlateau(optimizer, mode="min", factor=0.5, patience=2)
 
 contrastive_data = ContrastiveDataset(data, model, device)
 contrastive_loader = contrastive_data.get_dataloader(BATCH_SIZE)
@@ -53,7 +56,7 @@ for epoch in range(EPOCHS):
     epoch_time = time.time() - epoch_start_time
     remaining_time = epoch_time * (EPOCHS - (epoch + 1))
     print("\n" + "-" * 50)
-    print(f"Epoch: {epoch + 1}/{EPOCHS}\nLoss: {total_loss/len(contrastive_loader):.4f}")
+    print(f"Epoch: {epoch + 1}/{EPOCHS}\nLoss: {total_loss / len(contrastive_loader):.4f}")
     print(f"Time Taken: {epoch_time:.4f}s")
     print(f"Estimated Time to Finish: {remaining_time:.2f}s")
     print("-" * 50)
@@ -75,21 +78,20 @@ plt.hist(distances, bins=50, alpha=0.6, label="Distances")
 plt.legend(), plt.xlabel("Distance"), plt.ylabel("Frequency")
 plt.savefig("distances.svg", format="svg")
 
-# Testing Hyperparameters
-THRESHOLD = np.percentile(distances, 90)
+accuracy_metric = torchmetrics.Accuracy(task="binary").to(device)
+distances = []
+labels = []
 
 with torch.no_grad():
     for img1, img2, label in contrastive_loader:
         img1, img2, label = img1.to(device), img2.to(device), label.to(device)
-
         output1, output2 = model(img1, img2)
         distance = F.pairwise_distance(output1, output2)
+        distances.extend(distance.cpu().numpy())
+        labels.extend(label.cpu().numpy())
 
-        predictions = (distance < THRESHOLD).float()
-        label = label.squeeze()
-
-        correct += (predictions == label).sum().item()
-        total += label.numel()
-
-accuracy = 100 * correct / total
-print(f"Model Accuracy: {accuracy:.2f}%")
+THRESHOLD = threshold_otsu(np.array(distances))
+labels = torch.tensor(np.array(labels), dtype=torch.float32, device=device).squeeze()
+predictions = torch.tensor(np.array([1 if d < THRESHOLD else 0 for d in distances]), dtype=torch.float32, device=device)
+accuracy = accuracy_metric(predictions, labels)
+print(f"Model Accuracy: {accuracy.item() * 100:.2f}%")
