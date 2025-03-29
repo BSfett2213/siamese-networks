51e7365d3758b3bc4b6d60520f64c9c2073c38a4
draby2466 <ribhavyadav2466@gmail.com>
2025-03-29 10:22:22 +0530

    Faster _find_hard_negative implementation.

--- dataset/data_model.py
+++ dataset/data_model.py
@@ -21,22 +21,26 @@ class ContrastiveDataset(Dataset):
     def _find_hard_negative(self, anchor_img, anchor_label):
         different_labels = list(self.labels_to_indices.keys())
         different_labels.remove(anchor_label)
+
         hardest_negative = None
         min_distance = float("inf")
 
-        for neg_label in different_labels:
-            for neg_idx in self.labels_to_indices[neg_label]:
-                neg_img, _ = self.dataset[neg_idx]
+        self.model.eval()
+        with torch.no_grad():
+            anchor_emb = self.model.forward_image(anchor_img.to(self.device).unsqueeze(0))
 
-                with torch.no_grad():
-                    anchor_emb = self.model.forward_image(anchor_img.to(self.device).unsqueeze(0))
+            for neg_label in random.sample(different_labels, min(3, len(different_labels))):
+                neg_samples = random.sample(self.labels_to_indices[neg_label],
+                                            min(10, len(self.labels_to_indices[neg_label])))
+                for neg_idx in neg_samples:
+                    neg_img, _ = self.dataset[neg_idx]
                     neg_emb = self.model.forward_image(neg_img.to(self.device).unsqueeze(0))
                     distance = torch.dist(anchor_emb, neg_emb).item()
+                    if distance < min_distance:
+                        min_distance = distance
+                        hardest_negative = neg_idx
 
-                if distance < min_distance:
-                    min_distance = distance
-                    hardest_negative = neg_idx
-
+        self.model.train()
         return hardest_negative
 
     def __len__(self):
@@ -54,7 +58,7 @@ class ContrastiveDataset(Dataset):
         return img1, img2, torch.tensor([int(label1 != label2)], dtype=torch.float32)
 
     def get_dataloader(self, batch_size):
-        return DataLoader(self, batch_size=batch_size, shuffle=True)
+        return DataLoader(self, batch_size=batch_size, shuffle=True, num_workers=0)
 
 
 class TripletDataset(Dataset):
