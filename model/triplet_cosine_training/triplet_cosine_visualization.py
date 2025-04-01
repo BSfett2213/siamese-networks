0bb581abaea74e3d7c5779f896fe476d9b9545ab
draby2466 <ribhavyadav2466@gmail.com>
2025-04-01 16:40:42 +0530

    Added visualizations for triplet cosine training results.

---
+++ model/triplet_cosine_training/triplet_cosine_visualization.py
@@ -0,0 +1,27 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Set seaborn style
+sns.set_theme(style="whitegrid")
+palette = sns.color_palette("husl", 2)
+triplet_csv = pd.read_csv("./training_results.csv")
+
+# Plotting Triplet Loss
+plt.figure(figsize=(10, 6))
+sns.lineplot(x=triplet_csv['Epoch'], y=triplet_csv['Loss'], color=palette[0], label="Loss", linewidth=2.5, marker="o")
+plt.title("Triplet Training Loss per Epoch", fontsize=14, fontweight='bold')
+plt.xlabel("Epochs", fontsize=12), plt.ylabel("Loss", fontsize=12)
+plt.legend()
+plt.savefig("triplet_cosine_loss.svg", format='svg')
+plt.close()
+
+# Plotting Triplet Accuracy
+plt.figure(figsize=(10, 6))
+sns.lineplot(x=triplet_csv['Epoch'], y=triplet_csv['Accuracy'], color=palette[1], label="Accuracy", linewidth=2.5,
+             marker="o")
+plt.title("Triplet Training Accuracy per Epoch", fontsize=14, fontweight='bold')
+plt.xlabel("Epochs", fontsize=12), plt.ylabel("Accuracy", fontsize=12)
+plt.legend()
+plt.savefig("triplet_cosine_accuracy.svg", format='svg')
+plt.close()
